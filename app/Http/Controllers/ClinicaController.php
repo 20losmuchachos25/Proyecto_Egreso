48771eda61c2122210a3626c254b0d786cabaaca
Lucas Sequeira <lucas.sequeira@inumet.gub.uy>
2025-10-08 16:06:59 -0300

    Version Inicial - ClinicaAgregarCli

--- app/Http/Controllers/ClinicaController.php
+++ app/Http/Controllers/ClinicaController.php
@@ -9,4 +9,17 @@ class ClinicaController extends Controller
     public function ViewClinica(){
         return view('Clinica');
     }
+
+    public function AgregarClinica(Request $request){
+        $request->validate([
+            'Calle' => 'required|min:2',
+            'Numero' => 'required|integer|min:0',
+            'Esquina' =>'nullable',
+            'Referencia' => 'nullable'
+        ]);
+
+        $datos = $request->all();
+
+        $direccion = $datos['Calle'] . $datos['Numero'] . " esq " . $datos['Esquina'] . ". Referencia: " . $datos['Referencia'];
+    }
 }
--- app/Models/Clinica.php
+++ app/Models/Clinica.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Clinica extends Model
 {
-    //
+    protected $table='clinica';
+    protected $primaryKey='id';
+    public $timestamps=false;
+
+    protected $fillable = [
+        'Direccion'
+    ];
 }
